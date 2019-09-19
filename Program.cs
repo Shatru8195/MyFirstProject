5c322e26ead317a637d2c227d0b7d0a3f7263e56
SHATRUDHAN KUMAR <shatrudhan.kumar@webners.com>
2019-09-19 13:00:12 +0530

    Add files via upload
    How to read All Xml Tag, Attributes and their values using C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Assignment3_Lead_ITC_.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_Lead_ITC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ExecutionClass execution = new ExecutionClass();
+            //execution.AccessCode();
+
+            ReadXmlAllData readXmlAll = new ReadXmlAllData();
+            //readXmlAll.ReadXmlValueFun();
+            readXmlAll.ReadAllXmlValue();
+            Console.ReadLine();
+        }
+    }
+}
